eed163afa3892f2dcbb06a88f33e580c6f3fc978
Meliodas <31974197+Melioli@users.noreply.github.com>
2023-09-02 03:39:04 +0200

    I may have forgot this part

--- Tools/convertHI3PC.py
+++ Tools/convertHI3PC.py
@@ -52,35 +52,38 @@ class ConvertHonkaiImpactPlayerCharacter(Operator):
             bpy.context.object.display_type = 'WIRE'
 
         def GenShapekey():
-            #Generate A Shape Key
+           # Check if the required shape keys are present
+            if getKeyBlock("Mouth_A01") is None or getKeyBlock("Mouth_O01") is None or getKeyBlock("Mouth_Angry02") is None:
+                print("Required shape keys not found.")
+                return
+
+            # Generate A Shape Key
             bpy.ops.object.select_all(action='DESELECT')
             bpy.data.objects['Body'].select_set(True)
             bpy.context.view_layer.objects.active = bpy.data.objects['Body']
-            if getKeyBlock("Mouth_A01") is not None:      
-                bpy.data.shape_keys[getKeyBlock("Mouth_A01")].key_blocks["Mouth_A01"].value = 1.0
-                bpy.data.objects['Body'].shape_key_add(name="A", from_mix=True)
-                bpy.data.shape_keys[getKeyBlock("Mouth_A01")].key_blocks["Mouth_A01"].value = 0.0
+            bpy.data.shape_keys[getKeyBlock("Mouth_A01")].key_blocks["Mouth_A01"].value = 1.0
+            bpy.data.objects['Body'].shape_key_add(name="A", from_mix=True)
+            bpy.data.shape_keys[getKeyBlock("Mouth_A01")].key_blocks["Mouth_A01"].value = 0.0
 
-
-            #Generate O Shape Key
+            # Generate O Shape Key
             bpy.ops.object.select_all(action='DESELECT')
             bpy.data.objects['Body'].select_set(True)
             bpy.context.view_layer.objects.active = bpy.data.objects['Body']
-            if getKeyBlock("Mouth_O01") is not None:
-                bpy.data.shape_keys[getKeyBlock("Mouth_O01")].key_blocks["Mouth_O01"].value = 1.0
-                bpy.data.objects['Body'].shape_key_add(name="O", from_mix=True)
-                bpy.data.shape_keys[getKeyBlock("Mouth_O01")].key_blocks["Mouth_O01"].value = 0.0
+            bpy.data.shape_keys[getKeyBlock("Mouth_O01")].key_blocks["Mouth_O01"].value = 1.0
+            bpy.data.objects['Body'].shape_key_add(name="O", from_mix=True)
+            bpy.data.shape_keys[getKeyBlock("Mouth_O01")].key_blocks["Mouth_O01"].value = 0.0
 
-
-            #Generate CH Shape Key
+            # Generate CH Shape Key
             bpy.ops.object.select_all(action='DESELECT')
             bpy.data.objects['Body'].select_set(True)
             bpy.context.view_layer.objects.active = bpy.data.objects['Body']
-            if getKeyBlock("Mouth_Angry02") is not None:
-                bpy.data.shape_keys[getKeyBlock("Mouth_Angry02")].key_blocks["Mouth_Angry02"].value = 1.0
-                bpy.data.objects['Body'].shape_key_add(name="I", from_mix=True)
-                bpy.data.shape_keys[getKeyBlock("Mouth_Angry02")].key_blocks["Mouth_Angry02"].value = 0.0
+            bpy.data.shape_keys[getKeyBlock("Mouth_Angry02")].key_blocks["Mouth_Angry02"].value = 1.0
+            bpy.data.objects['Body'].shape_key_add(name="I", from_mix=True)
+            bpy.data.shape_keys[getKeyBlock("Mouth_Angry02")].key_blocks["Mouth_Angry02"].value = 0.0
+
+            # Create viseme shape keys
             bpy.ops.cats_viseme.create()
+
             bpy.ops.object.select_all(action='DESELECT')
 
         def FixEyes():
@@ -90,33 +93,37 @@ class ConvertHonkaiImpactPlayerCharacter(Operator):
             bpy.context.view_layer.objects.active = bpy.data.objects['Armature']
             bpy.ops.object.mode_set(mode='EDIT')
             bpy.ops.armature.select_all(action='DESELECT')
+
             def attacheyes(foot, toe):
                 if foot in armature.edit_bones and toe in armature.edit_bones:
                     foot_head = armature.edit_bones[foot].head
                     toe_head = armature.edit_bones[toe].head
                     foot_tail = armature.edit_bones[foot].tail
 
-                foot_tail.x = foot_head.x
-                foot_tail.y = foot_head.y
-                foot_tail.z = toe_head.z + 0.0012
-            
+                    foot_tail.x = foot_head.x
+                    foot_tail.y = foot_head.y
+                    foot_tail.z = toe_head.z + 0.12
+
             def move_eyebone(bone):
                 bone.select = True
                 bone.select_head = True
-                bone.select_tail = True     
-
-            attacheyes('Eye_R_End', 'Head')
-            attacheyes('Eye_L_End', 'Head')
+                bone.select_tail = True
 
             if "Eye_R_End" in armature.edit_bones:
+                attacheyes('Eye_R_End', 'Head')
                 move_eyebone(armature.edit_bones["Eye_R_End"])
-                bpy.ops.transform.translate(value=(0, 0.00035, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, True, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=True, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+                bpy.ops.transform.translate(value=(0, 0.035, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, True, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=True, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
                 bpy.ops.armature.select_all(action='DESELECT')
+            else:
+                print("Eye_R_End not found. Probably using an OldFace Model")
 
             if "Eye_L_End" in armature.edit_bones:
+                attacheyes('Eye_L_End', 'Head')
                 move_eyebone(armature.edit_bones["Eye_L_End"])
-                bpy.ops.transform.translate(value=(0, 0.00035, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, True, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=True, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+                bpy.ops.transform.translate(value=(0, 0.035, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, True, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=True, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
                 bpy.ops.armature.select_all(action='DESELECT')
+            else:
+                print("Eye_L_End not found. Probably using an OldFace Model")
 
             # Get the armature object and enter edit mode
             armature_obj = bpy.data.objects['Armature']
@@ -126,7 +133,7 @@ class ConvertHonkaiImpactPlayerCharacter(Operator):
 
             # Exit edit mode
             bpy.ops.object.mode_set(mode='OBJECT')
-         
+            
         def FixSpine():
             ob = bpy.data.objects['Armature']
             armature = ob.data
@@ -146,7 +153,7 @@ class ConvertHonkaiImpactPlayerCharacter(Operator):
             # Define the bone names and new names
             bone_names = ["Eye_L_End", "Eye_R_End"]
             new_names = ["EyeBone_L_A02", "EyeBone_R_A02"]
-            
+
             # Get the armature object
             armature = None
             for obj in bpy.context.scene.objects:
@@ -156,9 +163,18 @@ class ConvertHonkaiImpactPlayerCharacter(Operator):
 
             # Rename the bones
             if armature is not None:
-                for bone in armature.pose.bones:
-                    if bone.name in bone_names:
-                        bone.name = new_names[bone_names.index(bone.name)]
+                for bone_name in bone_names:
+                    if bone_name in armature.pose.bones:
+                        bone = armature.pose.bones[bone_name]
+                        index = bone_names.index(bone_name)
+                        if index < len(new_names):
+                            bone.name = new_names[index]
+                        else:
+                            print("New name not found for bone:", bone_name)
+                    else:
+                        print("Bone not found:", bone_name, "OldFace")
+            else:
+                print("Armature not found.")
 
         def ApplyTransforms():
             for ob in bpy.context.scene.objects:
