06e386ad941ac9a32bf4f34cc5303ea502fbbc13
Meliodas <31974197+Melioli@users.noreply.github.com>
2024-01-05 05:15:37 +0100

    Added a settings tab that'll allow for specific toggles. merge mesh is in there

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ from Hoyo2VRC.Tools.convertNPC import ConvertNonePlayerCharacter
 bl_info = {
     "name": "Hoyo2VRC",
     "author": "Meliodas, Mken",
-    "version": (1, 0, 7),
+    "version": (1, 0, 8),
     "blender": (3, 6, 2),
     "location": "3D View > Sidebar > Hoyo2VRC",
     "description": "Convert Hoyoverse rips to VRC",
@@ -56,34 +56,56 @@ class Hoyo2VRCModelPanel(Panel):
             )
             # Add a row for converting the avatar
             row = box.row()
+            split = row.split(factor=0.8)  # Adjust the factor to change the width of the main button
+            
             name = bpy.context.object.name.replace(".001", "").replace("_Render", "").replace("_merge", "").replace(" (merge)", "")
             if re.match(r"^(Cs_Avatar|Avatar|NPC_Avatar)_(Boy|Girl|Lady|Male|Loli)_(Sword|Claymore|Bow|Catalyst|Pole)_[a-zA-Z]+(?<!_\d{2})$", name):
                 # Genshin Impact Playable character
-                row.operator(
+                split.operator(
                     operator='hoyo2vrc.convertgpc',
                     text='Convert GI Avatar',
                     icon='PLAY'
                 )
             elif re.match(r"^(Avatar|Art|NPC_Avatar)_(Boy|Girl|Lady|Male|Kid|Lad|Maid|Miss|[a-zA-Z]+)?_?[a-zA-Z]*_(?<!_\d{2})\d{2}$", name):
                 # Honkai Star Rail Playable Character
-                row.operator(
+                split.operator(
                     operator='hoyo2vrc.converthsrpc',
                     text='Convert HSR Avatar',
                     icon='PLAY'
                 )
             elif re.match(r"^Avatar_\w+?_C\d+(_\w+)*$", name):
                 # Honkai Impact Playable Character
-                row.operator(
+                split.operator(
                     operator='hoyo2vrc.converthi3pc',
                     text='Convert HI3 Avatar',
                     icon='PLAY'
                 )
             else :
-                row.operator(
+                split.operator(
                     operator='hoyo2vrc.convertnpc',
                     text='Convert NPC',
                     icon='PLAY'
                 )
+                
+            # Add the toggle menu button to the right of the convert button
+            split.operator(Hoyo2VRCToggleMenuButton.bl_idname, text="", icon='SETTINGS')
+
+class Hoyo2VRCToggleMenu(bpy.types.Menu):
+    bl_idname = "HOYO2VRC_MT_toggle_menu"
+    bl_label = "Toggle Menu"
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.prop(context.scene, "merge_all_meshes", text="Merge All Meshes")
+
+class Hoyo2VRCToggleMenuButton(bpy.types.Operator):
+    bl_idname = "hoyo2vrc.toggle_menu_button"
+    bl_label = "Toggle Menu"
+
+    def execute(self, context):
+        bpy.ops.wm.call_menu(name=Hoyo2VRCToggleMenu.bl_idname)
+        return {'FINISHED'}
 
 class Hoyo2VRCSettingsPanel(Panel):
     bl_label = "Settings"
@@ -106,6 +128,7 @@ class Hoyo2VRCSettingsPanel(Panel):
             text='Install Dependencies',
             icon='ERROR'
         )
+        
 
 classes = [
     ConvertGenshinPlayerCharacter,
@@ -115,9 +138,23 @@ classes = [
     BetterFBXImport,
     BetterFBXExport,
     Hoyo2VRCModelPanel,
+    Hoyo2VRCToggleMenu,
+    Hoyo2VRCToggleMenuButton,
     Hoyo2VRCSettingsPanel,
     InstallDependencies
 
 ]
 
-register, unregister = bpy.utils.register_classes_factory(classes)
+def register():
+    bpy.types.Scene.merge_all_meshes = bpy.props.BoolProperty(
+        name="Merge All Meshes",
+        description="Decide whether all meshes should be merged or not",
+        default = False
+    )
+
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
