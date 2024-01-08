474cb5742e5047b278e04bcb704c4136e6b52dfa
Meliodas <31974197+Melioli@users.noreply.github.com>
2024-01-08 03:03:10 +0100

    Bump Version Number

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ from Hoyo2VRC.Tools.convertNPC import ConvertNonePlayerCharacter
 bl_info = {
     "name": "Hoyo2VRC",
     "author": "Meliodas, Mken",
-    "version": (1, 0, 8),
+    "version": (1, 0, 9),
     "blender": (3, 6, 2),
     "location": "3D View > Sidebar > Hoyo2VRC",
     "description": "Convert Hoyoverse rips to VRC",
