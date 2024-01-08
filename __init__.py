070473c503239147f6aab902a3abf152a4d94573
Meliodas <31974197+Melioli@users.noreply.github.com>
2024-01-08 07:03:20 +0100

    2 versions in 1 day is crazy

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ from Hoyo2VRC.Tools.convertNPC import ConvertNonePlayerCharacter
 bl_info = {
     "name": "Hoyo2VRC",
     "author": "Meliodas, Mken",
-    "version": (1, 0, 9),
+    "version": (1, 1, 0),
     "blender": (3, 6, 2),
     "location": "3D View > Sidebar > Hoyo2VRC",
     "description": "Convert Hoyoverse rips to VRC",
